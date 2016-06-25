cb3e61f851bc2f91b2ca07e16d1bd6f09e79deca
maryhoag <marymcevoyhoag@gmail.com>
2016-06-25 10:00:07 -0400

    added routing

--- app/routing/html_routes.js
+++ app/routing/html_routes.js
@@ -3,3 +3,9 @@
 //get route to /survey
 
 //default USE route to home.html
+// Basic route that sends the user first to the AJAX Page
+app.get('/', function(req, res){
+	
+	//res.send("Welcome to the Star Wars Page!")
+	res.sendFile(path.join(__dirname + '/home.html'));
+})
